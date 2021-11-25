b0f2aab6f3b008bcc853298ab61db87cd7b5f1e9
202002575 <eogkrry2020@gmail.com>
2021-11-25 09:34:52 +0900

    메인함수에 스택 테스트용 코드 뺌

--- termProject/main.cpp
+++ termProject/main.cpp
@@ -2,20 +2,6 @@
 #include "Stack.h"
 #include "AppController.cpp"
 int main() {
-	Stack<char> stack(10);
-    stack.push('a');
-    stack.push('b');
-    stack.push('c');
-    stack.push('d');
-    stack.push('e');
-    stack.push('f');
-
-    std::cout << stack.pop() << std::endl;
-    std::cout << stack.pop() << std::endl;
-    std::cout << stack.pop() << std::endl;
-    std::cout << stack.pop() << std::endl;
-    std::cout << stack.pop() << std::endl;
-
     AppController* appController = new AppController();
     appController->run();
 
